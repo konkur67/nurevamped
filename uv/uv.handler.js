87db3090c8ec78cedf5efb6415daf745db67546b
Konkur <faarisfarhan66@gmail.com>
2025-12-15 19:25:01 -0800

    Update uv.handler.js

--- uv/uv.handler.js
+++ uv/uv.handler.js
@@ -1,1 +1,16 @@
 
+self.addEventListener('install', (event) => event.waitUntil(self.skipWaiting()));
+self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
+
+self.addEventListener('fetch', (event) => {
+    const url = new URL(event.request.url);
+    const target = url.searchParams.get('url');
+
+    if (target) {
+        event.respondWith(fetch(target, {
+            method: event.request.method,
+            headers: event.request.headers
+        }));
+    }
+});
+
