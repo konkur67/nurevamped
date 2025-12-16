f0a64c715e6a9f8ea85841e9c763fb0119a8ff02
Konkur <faarisfarhan66@gmail.com>
2025-12-15 18:53:44 -0800

    Add UV configuration settings

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,1 +1,8 @@
-
+self.__uv$config = {
+  prefix: '/service/',
+  bare: 'https://petezahgames.com/bare/',
+  handler: '/uv/uv.handler.js',
+  bundle: '/uv/uv.bundle.js',
+  config: '/uv/uv.config.js',
+  sw: '/uv/uv.sw.js',
+};
