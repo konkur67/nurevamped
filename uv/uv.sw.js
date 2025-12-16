dec416d14237497cf8740a0bf07b4d70fc5c86e4
Konkur <faarisfarhan66@gmail.com>
2025-12-15 19:21:31 -0800

    Update uv.sw.js

--- uv/uv.sw.js
+++ uv/uv.sw.js
@@ -1,1 +1,21 @@
 
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    const url = new URL(event.request.url);
+    const targetUrl = url.searchParams.get('url');
+
+    if (targetUrl) {
+        event.respondWith(fetch(targetUrl, { 
+            method: event.request.method,
+            headers: event.request.headers
+        }));
+    }
+});
