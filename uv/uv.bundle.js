ccb43f1a7e68147931e654a57f506ad4c4b304c2
Konkur <faarisfarhan66@gmail.com>
2025-12-15 19:21:03 -0800

    Add iframe to load script based on URL query

--- uv/uv.bundle.js
+++ uv/uv.bundle.js
@@ -1,1 +1,16 @@
 
+const query = new URLSearchParams(window.location.search).get('q');
+const iframe = document.createElement('iframe');
+
+iframe.style.width = '100%';
+iframe.style.height = '100%';
+iframe.style.border = 'none';
+
+if (query) {
+  
+    iframe.src = `/uv.sw.js?url=${encodeURIComponent(query)}`;
+} else {
+    iframe.src = 'about:blank';
+}
+
+document.body.appendChild(iframe);
